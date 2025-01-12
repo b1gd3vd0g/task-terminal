c2bf75067d6cec85d5fda889a22a5f81da5c3e0c
b1gd3vd0g <devin@bigdevdog.com>
2025-01-11 16:57:09 -0800

    Export two functions: print_to_width and system_time_to_date_time

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,16 @@
-pub fn print_to_width(output: &str, width: usize) -> Result<(), ()> {
+use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::{DateTime, Utc};
+
+/// Prints an output String to the console, and also prints enough spaces so that
+/// it prints `width` characters. Returns Ok(()) in the case that output.len()
+/// is _less than_ `width`.
+/// 
+/// # Errors
+/// 
+/// Returns Err(()) in the case that output.len() > width. In this case, the first
+/// `width` characters are printed. The return value of this function can be ignored
+/// in order to view an imperfect table.
+pub fn print_to_width(output: String, width: usize) -> Result<(), ()> {
     if output.len() > width {
         // Error!
         let partial_output = &output[..width];
@@ -10,4 +22,24 @@ pub fn print_to_width(output: &str, width: usize) -> Result<(), ()> {
         print!(" ");
     }
     Ok(())
+}
+
+/// Converts a std::time::SystemTime to a chrono::DateTime<Utc>
+/// 
+/// # Errors
+/// Returns None if a a DateTime cannot be made from a timestamp (unlikely).
+pub fn system_time_to_date_time(t: SystemTime) -> Option<DateTime<Utc>> {
+    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(e) => {
+            let dur = e.duration();
+            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+            if nsec == 0 {
+                (-sec, 0)
+            } else {
+                (-sec - 1, 1_000_000_000 - nsec)
+            }
+        }
+    };
+    DateTime::from_timestamp(sec, nsec)
 }
